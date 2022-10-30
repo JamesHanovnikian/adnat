b7f8cc2cf12857b39a170e28e5140f2a28936364
YOUR NAME <jhanov@gmail.com>
2022-10-30 11:18:47 -0500

    fixes shifts table to reflect all shifts of given organization

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,16 +1,19 @@
 class ShiftsController < ApplicationController
   def index
     logged_in = current_user
-    shifts = Shift.where(user_id: 1)
+    organization_id = current_user.organization_id
+    shifts = Shift.where(organization_id: organization_id)
     render json: shifts.as_json
   end
 
   def create
+    organization = Organization.find_by(current_user.organization_id)
     shift = Shift.new(
       user_id = current_user.id,
       start = params[:start],
       finish = params[:finish],
-      break_length = params[:break_length]
+      break_length = params[:break_length],
+      organization_id = organization.id
     )
     shift.save
     render json: shift.as_json
